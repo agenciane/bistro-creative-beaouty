5fbf9d3065ae6e75f65f36718973c4662e16bc23
natalio silveira <nataliosilveira@yahoo.com.br>
2025-07-28 18:09:02 -0300

    feat: update images

--- src/app/pratos-quentes/page.tsx
+++ src/app/pratos-quentes/page.tsx
@@ -9,10 +9,10 @@ const entrada: React.FC = () => {
           <h2 className="text-5xl uppercase leading-16">Pratos quentes</h2>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          <ItemMenu title="Medalhão de Mignon com risoto cremoso" photoUrl="/medalhao-de-mignon.webp" />
-          <ItemMenu title="Fettuccine na manteiga e ervas com frango ou mignon" photoUrl="/fetutine-frango.webp" />
+          <ItemMenu title="Medalhão de Mignon com risoto cremoso" photoUrl="/risoto-ia.webp" />
+          <ItemMenu title="Fettuccine na manteiga e ervas com frango ou mignon" photoUrl="/fetutine-mignon.webp" />
           <ItemMenu title="Frango grelhado ao limão siciliano com risoto leve" photoUrl="/frango-grelhado-limao.webp" />
-          <ItemMenu title="Frango grelhado ou mignon com arroz branco e feijão" photoUrl="/arroz-feijao.webp" />
+          <ItemMenu title="Frango grelhado ou mignon com arroz branco e feijão" photoUrl="/frango-grelhado-ia.webp" />
         </div>
       </section>
     </main>
