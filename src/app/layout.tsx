60b972c995bc062530a053d3b319c372feb65231
natalio silveira <nataliosilveira@yahoo.com.br>
2025-07-28 22:46:00 -0300

    feat: update font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,11 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import localFont from 'next/font/local';
 import './globals.css';
 import Footer from './components/footer';
 import Header from './components/header';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+const okomito = localFont({
+  src: '../../public/Okomito-Medium.ttf',
 });
 
 export const metadata: Metadata = {
@@ -26,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black text-white`}>
+      <body className={`${okomito.className} ${okomito.style.fontFamily} antialiased bg-black text-white`}>
         <Header />
         {children}
         <Footer />
